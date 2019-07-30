a6f2068aad4aeee1a55bc48873c18982cdb6088a
Chirag <dawra1997@gmail.com>
2019-07-30 12:55:20 +0530

    last last commit

--- Calci.java
+++ Calci.java
@@ -19,7 +19,8 @@ public class Calci {
 	{
 		System.out.println("division");
 		System.out.println("Division is " + (x/y)  + "Master");
-		System.out.println("New mul divide");
+		System.out.println("New mul divide" + "one more" + "last commit mul divide");
+        	
 	}
 	
 }
