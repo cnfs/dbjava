b1684fe8f4e803b9fcfaefb428ac1193a9848560
Chirag <dawra1997@gmail.com>
2019-07-30 12:52:27 +0530

    last try commit

--- Calci.java
+++ Calci.java
@@ -12,11 +12,14 @@ public class Calci {
 	}
 	public void mul(double x,double y)
 	{
+		System.out.println("multiplication");
 		System.out.println("Multiplication is " + (x*y) );
 	}
 	public void div(double x,double y)
 	{
-		System.out.println("Division is " + (x/y) );
+		System.out.println("division");
+		System.out.println("Division is " + (x/y)  + "Master");
+		System.out.println("New mul divide");
 	}
 	
 }
